2ce8fd415f92be0ae476d8e82efaa8df8f342ddf
Ninjaneer87 <komodoutd@gmail.com>
2023-09-11 09:12:43 +0200

    replaced deprecated useNetwork with useChain

--- context/web3Context.tsx
+++ context/web3Context.tsx
@@ -1,13 +1,20 @@
 import React, { useContext, createContext, useEffect } from "react";
-import { useAddress, useContract, useNetwork } from '@thirdweb-dev/react';
+import { useAddress, useContract, useChain } from "@thirdweb-dev/react";
 import { ethers } from "ethers";
 import { SmartContract } from "@thirdweb-dev/sdk";
-import useWalletBalance, { Balance } from 'hooks/web3/useWalletBalance';
+import useWalletBalance, { Balance } from "hooks/web3/useWalletBalance";
 
 type Web3ContextType = {
   contract: SmartContract<ethers.BaseContract> | undefined;
   address: string | undefined;
-  network: ReturnType<typeof useNetwork>[0]['data']['chain'];
+  network:
+    | {
+        chain: string;
+        chainId: number;
+        name: string;
+        slug: string;
+      }
+    | undefined;
   balance: Balance | undefined;
 };
 
@@ -20,21 +27,19 @@ type Props = {
 export const Web3ContextProvider = ({ children }: Props) => {
   const { contract } = useContract(process.env.NEXT_PUBLIC_CONTRACT_ADDRESS);
   const address = useAddress();
-  const network = useNetwork();
+  const network = useChain();
   const { data: balance } = useWalletBalance(address);
 
   const context: Web3ContextType = {
     address,
     contract,
-    network: network[0]?.data?.chain,
-    balance
+    network,
+    balance,
   };
 
   return (
-    <Web3Context.Provider value={context} >
-      {children}
-    </Web3Context.Provider>
-  )
+    <Web3Context.Provider value={context}>{children}</Web3Context.Provider>
+  );
 };
 
 export const useWeb3Context = () => useContext(Web3Context) as Web3ContextType;
